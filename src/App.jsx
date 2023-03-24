0da610062de7ea6be65db16ed24a7a5c61299056
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-03-24 14:07:21 +0600

    feat: add tailwind and daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <button class="btn">Button</button>
     </div>
   );
 }
